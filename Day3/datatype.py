fb262cbaa7086c3ad8f701318c3730ae0729d71a
kiganyamburu <mburukiganya@gmail.com>
2025-01-21 16:06:22 +0300

    datatypes

---
+++ Day3/datatype.py
@@ -0,0 +1,11 @@
+failed_subjects = 2.44
+name = "John"
+boolean = True or False
+a = "it's"    # single quote
+b = 'it\'s'  # escape character
+print ("Dear Mrs Badger")
+print("Your son " + name + " is failing " + failed_subjects + " subjects.")
+print(name + " will need to redo " + failed_subjects + " courses.")
+name = "Eric"
+print (name + " is doing well in geometry.")
+
