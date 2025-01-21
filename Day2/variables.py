1badac447a39a17622eab1cd020a177cc545d6da
kiganyamburu <mburukiganya@gmail.com>
2025-01-21 15:53:29 +0300

    strings

--- Day2/variables.py
+++ Day2/variables.py
@@ -1,2 +1,9 @@
 # variables are like container that holds data
 
+failed_subjects = "2"
+name = "John"
+print("Dear Mrs. Jane,")
+print("Your son " + name + " is failing " + failed_subjects + " subjects.")
+print(name + " will need to redo " + failed_subjects + " courses.")
+name = "Eric"
+print(name + " is doing well in geography")
