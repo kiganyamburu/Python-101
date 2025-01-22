318614da4c4bf030e0c20900e6202d9ad58d1b4d
kiganyamburu <mburukiganya@gmail.com>
2025-01-22 12:47:29 +0300

    done

--- Day4/datatype.py
+++ Day4/datatype.py
@@ -1,3 +1,10 @@
 print('Variables & Datatypes - Exercise')
 #Create appropriate Variables for Item name, the price 
 #and how many you have in stock
+item_name = 'Laptop'
+price = 1000
+stock = 5
+#Print out the item name, price and stock
+print('Item Name:', item_name)
+print('Price:', price)
+print('Stock:', stock)
