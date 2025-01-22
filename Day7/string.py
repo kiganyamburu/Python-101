978b637f67268e4d3e626a1f4f4272626aef9719
kiganyamburu <mburukiganya@gmail.com>
2025-01-22 15:28:31 +0300

    exercise

---
+++ Day7/string.py
@@ -0,0 +1,4 @@
+# 1 From the sting "Welcome to Python 101: Strings", extract tex and create/print a new string that says *"1 Welcome RIng to Tyler" *Every first letter in a word should be capitalized(title format) 
+# 2. Print the same string backwards 
+
+
