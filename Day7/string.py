f36be138ca4bd39a29d0c2a39743eb48f087aeb8
kiganyamburu <mburukiganya@gmail.com>
2025-01-22 16:00:50 +0300

    revers

--- Day7/string.py
+++ Day7/string.py
@@ -2,3 +2,7 @@
 # 2. Print the same string backwards 
 
 
+msg = "Welcome to Python 101: Strings"
+new_msg = msg[0:7] + ' ' + msg[25:30]+' ' + msg[8:10]+' ' + msg[8] + msg[12] + msg[2] + msg[1] + msg[25]
+print(new_msg.title())
+print(new_msg[::-1])
