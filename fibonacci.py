b300ccb2fffccb41e7a7f3e434f5a8d12f28acb3
kiganyamburu <mburukiganya@gmail.com>
2025-02-03 11:51:18 +0300

    iterate fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    prev, curr = 0,1
+    for _ in range(2, n+1):
+        prev, curr = curr, prev + curr
+    return curr
+
+print(fibonacci(10))
