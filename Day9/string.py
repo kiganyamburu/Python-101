44a708d2712676491071007b57d62457c339b489
kiganyamburu <mburukiganya@gmail.com>
2025-02-04 11:32:25 +0300

    capitelize

--- Day9/string.py
+++ Day9/string.py
@@ -13,4 +13,6 @@ print("peter" in msg)
 name = "TERRY"
 color = "Red"
 msg2 = '[' + name + '] loves the color ' + color + ' ! '
+msg3 = f'[{name.capitalize()}] loves the color {color.lower()} !'
 print(msg2)
+print(msg3)
