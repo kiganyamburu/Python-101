168f6ea58e44b5d92a3a1c9805a8c5d87bd49b9d
kiganyamburu <mburukiganya@gmail.com>
2025-02-04 12:00:36 +0300

    comments

---
+++ Day10/distance.py
@@ -0,0 +1,6 @@
+# create a distance converter converting Km ro miles
+# Take two inputs from users:Their first name and the distance in Km
+# Print: Greet user by name and show Km, and miles values
+# 1 mile id 1.609 lm
+# hint: use correct types for calculation and print 
+# Did you capitalize the name
