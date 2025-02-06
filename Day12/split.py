fc4291323a8b2351abf9335c08212b2ff90cdf65
kiganyamburu <mburukiganya@gmail.com>
2025-02-06 19:10:19 +0300

    done

--- Day12/split.py
+++ Day12/split.py
@@ -2,4 +2,4 @@ msg = "welcome to Python 101: split and join"
 csv = "Eric,John,Michael,Terry,Graham"
 friends_list = ['Eric', 'John', 'Michael', 'Terry', 'Graham']
 print(msg.split())
-print(msg.split(" "))
+print(msg.split(" "), type(msg.split(" ")))
