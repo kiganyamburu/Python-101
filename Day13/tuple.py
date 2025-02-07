42709ccaf6d444101a0cc2429b9f049481d644de
kiganyamburu <mburukiganya@gmail.com>
2025-02-07 17:15:14 +0300

    everything to know about tuples

--- Day13/tuple.py
+++ Day13/tuple.py
@@ -1,4 +1,6 @@
-# tuples are list that can be changed 
+# tuples are list that cant not  be changed
+# tuples are immutable
+# Accessing tuple is easer the list and list  faster
 # Example of a tuple
 friends_tuples = ("john", "michael", "Terry", "eric", "graham")
 
@@ -7,3 +9,4 @@ friends = ['John', 'michael', 'Terry', 'Eric', 'Graham']
 
 
 print(friends)
+print(friends_tuples)
