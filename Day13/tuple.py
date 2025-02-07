8869a537e8b328a5a6d2d48a22e1896613032d9b
kiganyamburu <mburukiganya@gmail.com>
2025-02-07 16:21:02 +0300

    understanding tuple

---
+++ Day13/tuple.py
@@ -0,0 +1,9 @@
+# tuples are list that can be changed 
+# Example of a tuple
+friends_tuples = ("john", "michael", "Terry", "eric", "graham")
+
+# Example of a list
+friends = ['John', 'michael', 'Terry', 'Eric', 'Graham']
+
+
+print(friends)
