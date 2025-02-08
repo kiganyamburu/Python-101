33b24f77c9e2553f929aa38ab3a21390193c0728
kiganyamburu <mburukiganya@gmail.com>
2025-02-08 18:17:17 +0300

    intersect

--- Day13/sets.py
+++ Day13/sets.py
@@ -1,1 +1,7 @@
-friends_set = {""}
+# sets are unordered and they remove any duplicates elements
+# They are first in finding members in the sets 
+
+
+friends_set = {"John", "Michael", "Terry","Eric", "Graham"}
+my_friends_set = {"Reg", "loretta", "Colin", "Graham"}
+print(my_friends_set.intersection(my_friends_set))
