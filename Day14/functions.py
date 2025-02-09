8ef7c4bff34096fdc0451e3248811e0055c7d5ad
kiganyamburu <mburukiganya@gmail.com>
2025-02-09 14:35:16 +0300

    parameter

--- Day14/functions.py
+++ Day14/functions.py
@@ -1,8 +1,9 @@
 # Functions in programming are reusable blocks of code that perform a specific task. They help organize code, make it more readable, and avoid repetition.
 # always declare function before using them.
-# above where you are going to use them
+# above where you are going to use them.
+# A parameter in function is something like a variable 
 
-def greeting():
-    print("hello, world!")
+def greeting(name):
+    print("hello" + name + "!")
     
-greeting()
+greeting("Peter")      
