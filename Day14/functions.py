d90a655894f292345eff1cd14324d7ddace220f4
kiganyamburu <mburukiganya@gmail.com>
2025-02-09 14:31:08 +0300

    greeting

---
+++ Day14/functions.py
@@ -0,0 +1,8 @@
+# Functions in programming are reusable blocks of code that perform a specific task. They help organize code, make it more readable, and avoid repetition.
+# always declare function before using them.
+# above where you are going to use them
+
+def greeting():
+    print("hello, world!")
+    
+greeting()
