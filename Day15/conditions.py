2aedc5141398fa23dc0b02a21dbda75b34d96a77
kiganyamburu <mburukiganya@gmail.com>
2025-02-10 18:00:28 +0300

    both

--- Day15/conditions.py
+++ Day15/conditions.py
@@ -2,6 +2,6 @@ is_raining = True
 is_cold = False
 print("Good morning")
 if is_raining and is_cold:
-    print("Bring umbrela")
+    print("Bring umbrella ot jacket or both")
 else:
     print("Umbrella optional")
