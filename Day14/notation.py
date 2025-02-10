6bdf475736c52b407afea6be3c5e05374c5e3eba
kiganyamburu <mburukiganya@gmail.com>
2025-02-10 17:37:27 +0300

    removed the concatination

--- Day14/notation.py
+++ Day14/notation.py
@@ -1,7 +1,6 @@
 def greeting(name, age=28, color="red"):
  #Greets user with “name” from “input box” and “age”, if available, default age is used   
-   print("Hello "  +  name.capitalize() + ", you will be " + str(age+1) +" next birthday!")
-   print(f"Hello {name.capitalize()}, you will be {age+1} next birthday!")
-   print(f"We hear you like the color {color.lower()}!")
+    print(f"Hello {name.capitalize()}, you will be {age+1} next birthday!")
+    print(f"We hear you like the color {color.lower()}!")
 
 greeting("brian", 27,"Blue")
