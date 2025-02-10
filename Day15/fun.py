29d8f10b4b30d9550f12b6f805cd1bdde018eded
kiganyamburu <mburukiganya@gmail.com>
2025-02-10 19:18:39 +0300

    fer

---
+++ Day15/fun.py
@@ -0,0 +1,16 @@
+def calc():
+    temp_cel = 100
+    temp_fer = 20
+    convert_from_cel_to_fer = temp_cel* 9/5 + 32
+    print(convert_from_cel_to_fer)
+calc()
+    
+    
+    
+    
+    
+    
+    
+    
+# Bonus: Extend functionality with extra mode so it also does celsius to fahrenheit conversion
+# formula is: temp in C*9/5 + 32 = temp in 
