fbf6f4a8112caa3f15a4c5bdc133401ecb42aa72
kiganyamburu <mburukiganya@gmail.com>
2025-02-10 18:27:16 +0300

    sample

--- Day15/exe.py
+++ Day15/exe.py
@@ -1,4 +1,17 @@
-print('if elif else - Exercise')
+num1 = 4
+num2 = 2
+sum = num1 + num2
+subtract = num1 - num2
+multiply = num1 + num2
+divide = num1 + num2
+modulus = num1 % num2
+print(modulus)
+
+
+
+
+
+
 # Create a calculator which handles +,-,*,/ and outputs answer based on the mode/ operator used
 # Hint: use 3 separate inputs 
 # Bonus: Extend functionality with extra mode so it also does celsius to fahrenheit conversion
