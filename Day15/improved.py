4170c56ff3dff52207f6a42bcfaca8ea32ad40bd
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 00:08:24 +0300

    optimized

--- Day15/improved.py
+++ Day15/improved.py
@@ -1,30 +1,24 @@
 def num_days(month):
 
-    if month == 'jan':
+    if (
+        month == "jan"
+        or month == "mar"
+        or month == "may"
+        or month == "may"
+        or month == "jul"
+        or month == "nov"
+        or month == "dec"
+        or month == "aug"
+    ):
         print(f"number of days in {month} is 31")
-    elif month == 'feb':
-        print(f'number of days in {month} is 28')
-    elif month == 'mar':
-        print(f'number of days in {month} is 31')
-    elif month == 'apr':
-        print(f'number of days in,{month},is,30')
-    elif month == 'may':
-        print(f'number of days in,{month},is,31')
-    elif month == 'jun':
-        print(f'number of days in,{month},is,30')
-    elif month == 'jul':
-        print(f'number of days in,month, is 31')
-    elif month == 'aug':
-        print('number of days in',{month},'is',31)
-    elif month == 'sep':
-        print('number of days in',{month},'is',30)
-    elif month == 'oct':
-        print('number of days in',{month},'is',31)
-    elif month == 'nov':
-        print('number of days in',{month},'is',30)
-    elif month == 'dec':
-        print(f'number of days in,{month},is,31')
 
-num_days('jan')
+    elif month == "apr" or month == "jun" or month == "sep" or month == "nov":
+        print(f"number of days in,{month},is,30")
+
+    else:
+        print(f"number of days in {month} is 28")
+
+
+num_days("feb")
 # optimize/shorten the code in the function
-# try to reduce the number of conditionals 
+# try to reduce the number of conditionals
