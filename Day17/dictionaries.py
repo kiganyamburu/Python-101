707422f2dd7d4a9d2e4b9bc298dc553fa76a2b29
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 13:35:15 +0300

    .get() method

--- Day17/dictionaries.py
+++ Day17/dictionaries.py
@@ -1,9 +1,18 @@
 # they store key and pair value
-people = {
-    "John": 20,
-    "Ken": 21,
-}
-print(type(people))
+# people = {
+#     "John": 20,
+#     "Ken": 21,
+# }
+# print(type(people))
 
 
 # dictionaries are mutable so that means we can easily modify them
+
+movie = {
+    "title": "Inception",
+    "year": 2010,
+    "genre": "Action",
+    "cast": ["Peter", "John", "Erich", "george", "Terry"],
+}
+
+print(movie.get("genre"))
