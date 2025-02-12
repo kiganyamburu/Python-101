91741f47c94807aecbc2102368ddb799d80dcad6
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 12:15:45 +0300

    save

--- Day16/forloopexe.py
+++ Day16/forloopexe.py
@@ -1,4 +1,14 @@
 names = ["john ClEEse", "Eric IDLE", "michael"]
 names1 = ["graHam chapman", "TERRY", "terry jones"]
-for name in names:
-    print(f"{names.index(0)} you are invited to the party on saturday  ")
+
+# combine the two lists
+
+new_list = names + names1
+
+for _ in range(2):
+    new_list = input("Enter a name: ")
+    new_list.append(new_list)
+
+    for name in new_list:
+        formatted_name = name.title()
+        print(f"{formatted_name}! You are invited to the party o saturday")
