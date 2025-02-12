c7c078ad2b05821b635cf19eda741e99f556ae2a
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 11:26:45 +0300

    using in for loop to loop a list

--- Day16/forloops.py
+++ Day16/forloops.py
@@ -1,3 +1,15 @@
 for letter in range(1, 15, 3):
     print(letter)
 print("for loop done!")
+
+
+# working with loops in a list of name
+
+for name in [
+    "john",
+    "Terry",
+    "Eric",
+    "Michael",
+    "George",
+]:
+    print(name)
