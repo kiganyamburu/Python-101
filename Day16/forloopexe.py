67ee094514ea2d5b7cd72302888957f21e78d275
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 12:03:42 +0300

    list

---
+++ Day16/forloopexe.py
@@ -0,0 +1,4 @@
+names = ["john ClEEse", "Eric IDLE", "michael"]
+names1 = ["graHam chapman", "TERRY", "terry jones"]
+for name in names:
+    print(f"{names.index(0)} you are invited to the party on saturday  ")
