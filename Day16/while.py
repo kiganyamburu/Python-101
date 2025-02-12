8ca9ba6ffbed0c88b6dacf2a3278be2f45cbca81
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 10:33:09 +0300

    star

---
+++ Day16/while.py
@@ -0,0 +1,24 @@
+# while condition:
+#     code
+#     iteration
+
+
+# three loop Questions:
+# 1. What do i want to repeat
+#      message
+# 2. What do i want tp change each Time
+#      stars
+# 3. How long should we repeat
+#      5 times
+
+
+print("1.*Loops are great*")
+print("2.**Loops are great**")
+print("3i .***Loops are great***")
+print("4.****Loops are great****")
+print("5.*****Loops are great*****")
+
+i = 0
+while i < 5:
+    print("loops are awesome")
+    i = i + 1
