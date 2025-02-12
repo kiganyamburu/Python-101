127bbc0db7e2575d6ca76c26549b1f0985a3369f
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 10:36:49 +0300

    understanding while loop

--- Day16/while.py
+++ Day16/while.py
@@ -12,13 +12,7 @@
 #      5 times
 
 
-print("1.*Loops are great*")
-print("2.**Loops are great**")
-print("3i .***Loops are great***")
-print("4.****Loops are great****")
-print("5.*****Loops are great*****")
-
 i = 0
 while i < 5:
-    print("loops are awesome")
+    print(f"{i}." + "*" * i + "loops are awesome" + "*" * i)
     i = i + 1
