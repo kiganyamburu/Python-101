a08b52622173a0ea7af99b45e28b1217ed10fbef
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 11:41:07 +0300

    experimenting with nested loops

---
+++ Day16/nestedloop.py
@@ -0,0 +1,6 @@
+friends = ["John", "Terry", "Eric", "Michael", "George"]
+for friend in friends:
+    for number in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        print("friend, number")
+
+print("For Loop done!")
