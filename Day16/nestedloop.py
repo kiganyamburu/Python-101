1f9860c898a5dbf6a92c5c92e5101b4898398d94
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 11:43:20 +0300

    indentation

--- Day16/nestedloop.py
+++ Day16/nestedloop.py
@@ -1,6 +1,5 @@
 friends = ["John", "Terry", "Eric", "Michael", "George"]
 for friend in friends:
-    for number in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    for number in [1, 2, 3, 4, 5]:
         print("friend, number")
-
-print("For Loop done!")
+    print("For Loop done!")
