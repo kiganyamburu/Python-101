1588f36a581dab97fd649f72c458e61a9c6f79b7
kiganyamburu <mburukiganya@gmail.com>
2025-02-12 12:29:28 +0300

    enumarate function

--- Day16/Enumerate.py
+++ Day16/Enumerate.py
@@ -1,2 +1,10 @@
 print("python101 - Enumerate")
 friends = ["Brian", "Judith", "Reg", "Loretta", "Colin"]
+
+# i = 0
+# for friend in friends:
+#     print(i, friend)
+#     i = i + 1
+
+for num, friend in enumerate(friends, 1):
+    print(num, friend)
