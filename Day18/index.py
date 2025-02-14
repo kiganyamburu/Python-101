261ba76c62b159afaa41d8a15a9657503b33c2a1
kiganyamburu <mburukiganya@gmail.com>
2025-02-14 19:18:44 +0300

    index.html

--- Day18/index.py
+++ Day18/index.py
@@ -1,15 +1,15 @@
 
-my_file = open("greeting.txt","r")
-print(my_file.read(10))
-print(my_file.readlines())
-print(my_file.readlines())
+# my_file = open("greeting.txt","r")
+# print(my_file.read(10))
+# print(my_file.readlines())
+# print(my_file.readlines())
 
-line_by_line = my_file.readline()
-line_by_line = my_file.readline().splitlines()
-print("readline: ", line_by_line)
-print("splitlines: ", line_by_line)
+# line_by_line = my_file.readline()
+# line_by_line = my_file.readline().splitlines()
+# print("readline: ", line_by_line)
+# print("splitlines: ", line_by_line)
 
-my_file.close()
+# my_file.close()
 #  or 
 
 # context manager
