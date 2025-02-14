8620fd240a0d56abce996633f4796b93d0a87b49
kiganyamburu <mburukiganya@gmail.com>
2025-02-14 19:15:16 +0300

    friend

--- Day18/index.py
+++ Day18/index.py
@@ -1,4 +1,20 @@
+
 my_file = open("greeting.txt","r")
 print(my_file.read(10))
 print(my_file.readlines())
 print(my_file.readlines())
+
+line_by_line = my_file.readline()
+line_by_line = my_file.readline().splitlines()
+print("readline: ", line_by_line)
+print("splitlines: ", line_by_line)
+
+my_file.close()
+#  or 
+
+# context manager
+
+with open("friends.csv","r") as f:
+    print(f.read())
+    friends = f.read().splitlines()
+    print(friends)
