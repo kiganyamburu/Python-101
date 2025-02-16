fe7c3a0913a4acb284f7b7573a232e458f6afce4
kiganyamburu <mburukiganya@gmail.com>
2025-02-16 22:48:26 +0300

    list of movies

--- Day19/class.py
+++ Day19/class.py
@@ -21,5 +21,5 @@ class Movie:
 film_1 = Movie("Life of brian", 1979,8.1, True)
 film_2 = Movie("The Holy Grail", 1975,8.2, True)
 
-print(film_1.title, film_1.imdb_score)
-film_2.nice_print()
+films = [film_1, film_2]
+
