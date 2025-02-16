533569672b8f7687dd9ff7b051162fefcca7fb31
kiganyamburu <mburukiganya@gmail.com>
2025-02-16 22:34:58 +0300

    example of movie class

--- Day19/class.py
+++ Day19/class.py
@@ -3,3 +3,12 @@ print("Classes & objects")
 # object are the actual things built
 # variables re the attributes
 # funtions are the methods
+
+
+class Movie:
+    def __init__(self, title, year,imdb_score, have_seen):
+        self.title = title
+        self.year = year
+        self.imdb_score = imdb_score
+        self.have_seen = have_seen
+        pass
