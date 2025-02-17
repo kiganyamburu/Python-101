9d9691e64eafa00b3da71acae7715a848c6fc316
kiganyamburu <mburukiganya@gmail.com>
2025-02-17 22:20:56 +0300

    more things so be done with class

--- Day19/inheritance.py
+++ Day19/inheritance.py
@@ -5,5 +5,14 @@ class Person:
     def rest(elf):
         print("Gains 4 health points ")
         
-character1 = Person()
-character1.move
+class Doctor(Person):
+    def heal(self):
+        print("Heals 10 health points")
+class Fighter(Person):
+    def fight(self):
+        print("Do 10 health points of damage")
+    def move(self):
+        print("move 6 paces")
+        
+character1 = Fighter()
+character1.move()
