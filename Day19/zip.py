1ce70afa4dc2eb8fc22177bd768b27cfee0b27fb
kiganyamburu <mburukiganya@gmail.com>
2025-02-17 22:50:14 +0300

    new dict

--- Day19/zip.py
+++ Day19/zip.py
@@ -1,5 +1,18 @@
-nums = [1,2,3,4,5]
-letters = ["a", "b", "c", "d", "e"]
-names = ["John", "Eric", "Michael", "Graham", "Joe"]
-combo = list(zip(nums, letters))
-print(combo)
+# nums = [1,2,3,4,5]
+# letters = ["a", "b", "c", "d", "e"]
+# names = ["John", "Eric", "Michael", "Graham", "Joe"]
+# combo = list(zip(nums, letters, names))
+# print(combo)
+
+
+
+keys = "this parrot is deceased"
+values = "denna papegojan ar avlaiden"
+keys = keys.split()
+values = values.split()
+print(keys,values)
+en_sv_dict = dict(zip(keys, values))
+print(en_sv_dict)
+new_dict = {key:value for key, value in zip(keys, values)}
+print(new_dict)
+en,sv = list(en_sv_dict.keys())
