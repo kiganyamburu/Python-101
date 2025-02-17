19565c2cf73340ac9828f01e2abd91b3c212925e
kiganyamburu <mburukiganya@gmail.com>
2025-02-17 22:12:44 +0300

    class person

--- Day19/inheritance.py
+++ Day19/inheritance.py
@@ -1,1 +1,6 @@
-print("class inheritance")
+# print("class inheritance")
+class Person:
+    def move(self):
+        print("move 4 paces")
+    def rest(elf):
+        print("Gains 4 health points ")
