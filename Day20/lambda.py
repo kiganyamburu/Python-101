03bcc488cbdec591644d78f1e8ae5e32c98ec240
kiganyamburu <mburukiganya@gmail.com>
2025-02-18 20:22:27 +0300

    one line funtion

--- Day20/lambda.py
+++ Day20/lambda.py
@@ -5,8 +5,7 @@
 # square1 = lambda x : x*x
 # print(square(3))
 
-def nam_and_alias(name, alias):
-    return name.strip().title() + ":" + alias.strip().title()
+def nam_and_alias(name, alias): return name.strip().title() + ":" + alias.strip().title()
 name_and_alias1 = lambda name, alias: name.strip().title() + ":" + alias.strip().title()
 
 print(name_and_alias1("john ClEEse ","HECKLER"))
