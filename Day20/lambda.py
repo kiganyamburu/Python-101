1bef84174d723cb25c820bc1e017e9cfb82708c1
kiganyamburu <mburukiganya@gmail.com>
2025-02-18 20:17:03 +0300

    name

--- Day20/lambda.py
+++ Day20/lambda.py
@@ -1,8 +1,13 @@
-print(" lambda Function")
-def square(x):
-    return x * x
-# name = lambda parameter:expression
-square1 = lambda x : x*x
+# print(" lambda Function")
+# def square(x):
+#     return x * x
+# # name = lambda parameter:expression
+# square1 = lambda x : x*x
+# print(square(3))
 
+def nam_and_alias(name, alias):
+    return name.strip().title() + ":" + alias.strip().title()
+name_and_alias1 = lambda name, alias: name.strip().title() + ":" + alias.strip().title()
 
-print(square(3))
+print(name_and_alias1("john ClEEse ","HECKLER"))
+print(name_and_alias1("john ClEEse ","HECKLER"))
