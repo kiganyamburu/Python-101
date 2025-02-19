8e12b7a74e51e59e4808af044bcca70c57558411
kiganyamburu <mburukiganya@gmail.com>
2025-02-19 20:50:41 +0300

    indentation

---
+++ Day20/test1.py
@@ -0,0 +1,13 @@
+ Complete the 'minNum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER samDaily
+#  2. INTEGER kellyDaily
+#  3. INTEGER difference
+#
+
+def minNum(samDaily, kellyDaily, difference):
+    # Write your code here
+
+if __name__ == '__main__':
---
+++ Day20/test2.py
@@ -0,0 +1,30 @@
+import math
+import os
+import random
+import re
+import sys
+import requests
+
+
+
+#
+# Complete the 'finestFoodOutlet' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING city
+#  2. INTEGER votes
+# API URL https://jsonmock.hackerrank.com/api/food_outlets?city={city}&page={page_no}
+#
+
+def finestFoodOutlet(city, votes):
+    # Write your code here
+
+if __name__ == '__main__':
+
+    city = input()
+
+    votes = int(input().strip())
+
+    result = finestFoodOutlet(city, votes)
+
