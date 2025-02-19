75f2a39a859a85a8fd2bda15029a112309dbbf51
kiganyamburu <mburukiganya@gmail.com>
2025-02-19 21:06:15 +0300

    monty_python

--- Day20/lambda2.py
+++ Day20/lambda2.py
@@ -1,1 +1,7 @@
 monty_python = ["John Marwood Cleese", "Eric Idle", "Michael Edward Palin", "Terrence Vance Gilliam", 'Terry Graham Perry Jones', 'Graham Arthur Chapman']
+def sort_name(name):
+    return name(name)
+monty_python.sort(key = lambda name:name.split(" ")[-1])
+print(monty_python)
+monty_python.sort(key= sort_name)
+print(monty_python)
