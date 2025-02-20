0d27b8d108a058566b7b228aae863e040b607a86
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 19:00:16 +0300

    working with class

---
+++ Day20/exercise3.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        
+        
+Eric = Player("Eric", 116700)
+John = Player("John", 24327)
+Terry = Player("Terry", 150000)
+player_list = ["Eric", "John", "Terry"]
