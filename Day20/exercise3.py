3ec0f5b538c841418c20149504829f9ac4ba4884
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 19:03:46 +0300

    debugging

--- Day20/exercise3.py
+++ Day20/exercise3.py
@@ -8,3 +8,6 @@ Eric = Player("Eric", 116700)
 John = Player("John", 24327)
 Terry = Player("Terry", 150000)
 player_list = ["Eric", "John", "Terry"]
+sorted_players = sorted(player_list, key=lambda player: Player.score, reverse=True)
+
+print(Player.name for player in player_list)
