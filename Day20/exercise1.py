9b6abe9c2f45de776178426fdd1a8032199b1a54
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 18:30:47 +0300

    understanding lamda

--- Day20/exercise1.py
+++ Day20/exercise1.py
@@ -2,4 +2,13 @@ print("Lambdas exercise")
 
 
 def f(x): return x + 5
+# f = lambda x: x + 5
 print(f(2))
+
+
+
+def strip_spaces(str):
+    return "str".join(str.split(" "))
+
+strip_spaces1 = lambda str:" ".join(str.split(""))
+print(strip_spaces("monty python flaying circus"))
