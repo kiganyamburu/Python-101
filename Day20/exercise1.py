92c0a8192c31132412aebacf79c3dc44a722c17c
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 18:44:22 +0300

    exercise1 done

--- Day20/exercise1.py
+++ Day20/exercise1.py
@@ -22,3 +22,24 @@ list_a = [1,2,3,4]
 list_b = [3,4,5,6, 7]
 join_list_no_duplicates1  = lambda list_a, list_b: list(set(list_a + list_b))
 print(join_list_no_duplicates(list_a, list_b))
+
+
+
+# complete
+#Complete the function so it returns a function
+#def create_quad_func(a,b,c):
+#    '''return function f(x) = ax^2 + bx + c'''
+#    return lambda x:
+#f = create_quad_func(2,4,6)
+#g = 
+#print(f(2))
+#print(g(2))
+def create_quad_func(a, b, c):
+    '''return function f(x) = ax^2 + bx + c'''
+    return lambda x: a*x**2 + b*x + c
+
+f = create_quad_func(2, 4, 6)
+g = create_quad_func(1, -3, 2)  # Example of a different quadratic
+
+print(f(2))  # Output: 22 (2*2^2 + 4*2 + 6)
+print(g(2))  # Output: 0 (1*2^2 - 3*2 + 2)
