13248ce2a8439f0c4bfb60652746e38aa4395fec
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 18:17:43 +0300

    unpacking

--- Day20/lambda3.py
+++ Day20/lambda3.py
@@ -11,5 +11,7 @@ print(walkin_cost(2))
 print(price_calc(1,25)(2))
 
 
-print(lambda a,b,c:a+b+c)(2,3,4)
-print(lambda a,b,c=2:a+b+c)(2,3,)
+print((lambda a,b,c:a+b+c)(2,3,4))
+print((lambda a,b,c=2:a+b+c)(2,3,))
+print((lambda a,b,c=2:a+b+c)(2,c=3,b=4))
+print((lambda *args: sum(args))(2,3,4, 50))
