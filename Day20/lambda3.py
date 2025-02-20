63131afb0986abe838df19d478bd8f6f21838e47
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 18:12:11 +0300

    lambda funtion allows you to call the print and call the funtion

--- Day20/lambda3.py
+++ Day20/lambda3.py
@@ -1,15 +1,14 @@
-def func(n):
-    return lambda a: a * n
-# a*2
-doubler = func(2)
-print(doubler(3))
-quintiple = func(5)
-print(quintiple(3))
+
 
 
 def price_calc(start, hourly_rate):
     return lambda hours: start + hourly_rate * hours
 
 walkin_cost = price_calc(10, 30)
-
+premium_cost = price_calc(1,25)
+print(premium_cost(2))
 print(walkin_cost(2))
+print(price_calc(1,25)(2))
+
+
+print(lambda a,b,c:a+b+c)(2,3,4)
