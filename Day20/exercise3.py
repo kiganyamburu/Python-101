06ff0beebfdf1018c6d15416a8258c5175c7d5fc
kiganyamburu <mburukiganya@gmail.com>
2025-02-20 19:07:06 +0300

    done

--- Day20/exercise3.py
+++ Day20/exercise3.py
@@ -1,13 +1,15 @@
 class Player:
     def __init__(self, name, score):
-        self.name = name
+        self.name = name  # Fixed the typo here
         self.score = score
-        
-        
-Eric = Player("Eric", 116700)
-John = Player("John", 24327)
-Terry = Player("Terry", 150000)
-player_list = ["Eric", "John", "Terry"]
-sorted_players = sorted(player_list, key=lambda player: Player.score, reverse=True)
 
-print(Player.name for player in player_list)
+Eric = Player('Eric', 116700)
+John = Player('John', 24327)
+Terry = Player('Terry', 150000)
+player_list = [Eric, John, Terry]
+
+# Sort by score using lambda
+sorted_players = sorted(player_list, key=lambda player: player.score, reverse=True)  # Added reverse=True for descending order
+
+
+print([player.name for player in sorted_players]) # Output: ['Terry', 'Eric', 'John']
