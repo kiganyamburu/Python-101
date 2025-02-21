854f2394371f9a7fb077888e2431afccf12bb1ac
kiganyamburu <mburukiganya@gmail.com>
2025-02-21 22:38:35 +0300

    new way of writing list

--- Day21/comprehensionlist.py
+++ Day21/comprehensionlist.py
@@ -1,6 +1,10 @@
 numbers = [1,2,3,4,5,6,7,8,9]
 # give me a list with num squared for each num in numbers
+
 new_lists = []
 for num in numbers:
     new_lists.append(num*num)
+print(new_lists)
+
+new_lists = [num*num for num in numbers]
 print(new_lists)
