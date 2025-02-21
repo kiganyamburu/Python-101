7a44ca042041cc49e9c9e2f6905f4261f180d292
kiganyamburu <mburukiganya@gmail.com>
2025-02-21 22:46:08 +0300

    odd

--- Day21/comprehensionlist.py
+++ Day21/comprehensionlist.py
@@ -8,6 +8,12 @@ print(new_lists)
 
 new_lists = [num for num in numbers if num % 2 == 0]
 print(new_lists)
+# even number
 
+new_lists = [num for num in numbers if num % 2 == 1]
+print(new_lists)
+# odd number 
 # give me a list with num for each num in numbers if num is even
 
+new_lists = [num for num in numbers if num % 2 != 1]
+print(new_lists)
