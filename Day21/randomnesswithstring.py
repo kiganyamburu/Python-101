e93595ef15136759076268ab051df49d17592f07
kiganyamburu <mburukiganya@gmail.com>
2025-02-22 21:11:29 +0300

    word

--- Day21/randomnesswithstring.py
+++ Day21/randomnesswithstring.py
@@ -1,4 +1,4 @@
-import random
+import random, string
 # friends_list = ["John", "Eric", "Michael", "Terry", "Graham"]
 # print(random.sample(friends_list,5))
 # random.shuffle(friends_list)
@@ -7,4 +7,11 @@ import random
 smallcaps = 'abcdefghijklmnopqrstuvwxyz'
 largecaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 digits = '0123456789'
-print()
+letters_numbers = string.ascii_letters + string.digits
+word = " "
+for i in range(7):
+    word =+ random.choice(letters_numbers)
+word1 = "".join(random.sample(letters_numbers,7))
+
+print(word)
+print(word1)
