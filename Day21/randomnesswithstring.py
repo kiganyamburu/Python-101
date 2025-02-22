950e851fecff6a44d1d811a49a4f3269b7c68cbe
kiganyamburu <mburukiganya@gmail.com>
2025-02-22 21:07:05 +0300

    new

--- Day21/randomnesswithstring.py
+++ Day21/randomnesswithstring.py
@@ -1,5 +1,10 @@
 import random
-friends_list = ["John", "Eric", "Michael", "Terry", "Graham"]
-print(random.sample(friends_list,5))
-random.shuffle(friends_list)
-print(friends_list)
+# friends_list = ["John", "Eric", "Michael", "Terry", "Graham"]
+# print(random.sample(friends_list,5))
+# random.shuffle(friends_list)
+# print(friends_list)
+
+smallcaps = 'abcdefghijklmnopqrstuvwxyz'
+largecaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+digits = '0123456789'
+print()
