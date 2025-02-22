4df794dfe6f420e7cb404a1f7c351f03d9627be2
kiganyamburu <mburukiganya@gmail.com>
2025-02-22 20:25:23 +0300

    zip the two lists

--- Day21/comprehensiondictionary.py
+++ Day21/comprehensiondictionary.py
@@ -2,3 +2,4 @@
 movies = ["And now for something completely Different", "Monty Python and the Holy Grail","monty Python's life of Brian", "Monty python live at the Hollywood Bowl","Monty Python's the meaning of life", "Monty Python live (mostly)"]
 year = [1971, 1975,1979, 1982, 1983, 2014]
 names = ["John", "Eric", "michael", "Graham", "Terry", "TerryG"]
+print(list(zip(movies, year)))
