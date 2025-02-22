4f5739ae78d52cafa151a69d6bdde53bf04114c3
kiganyamburu <mburukiganya@gmail.com>
2025-02-22 20:23:44 +0300

    working with dic

--- Day21/comprehensiondictionary.py
+++ Day21/comprehensiondictionary.py
@@ -1,2 +1,4 @@
 #  Dictionary comprehensions 
-movies = ["And now for something completely Different", "Monty Python and the Holy Grail",""]
+movies = ["And now for something completely Different", "Monty Python and the Holy Grail","monty Python's life of Brian", "Monty python live at the Hollywood Bowl","Monty Python's the meaning of life", "Monty Python live (mostly)"]
+year = [1971, 1975,1979, 1982, 1983, 2014]
+names = ["John", "Eric", "michael", "Graham", "Terry", "TerryG"]
