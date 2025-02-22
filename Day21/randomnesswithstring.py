06c602b79b32a047c90ef4b49bba8aef856fe3cd
kiganyamburu <mburukiganya@gmail.com>
2025-02-22 21:12:56 +0300

    pw genarator

--- Day21/randomnesswithstring.py
+++ Day21/randomnesswithstring.py
@@ -10,7 +10,7 @@ digits = '0123456789'
 letters_numbers = string.ascii_letters + string.digits
 word = " "
 for i in range(7):
-    word =+ random.choice(letters_numbers)
+    word += random.choice(letters_numbers)
 word1 = "".join(random.sample(letters_numbers,7))
 
 print(word)
