ed6ab7aec0df0ef73623edd253e6e10f97fd8cd0
kiganyamburu <mburukiganya@gmail.com>
2025-02-23 19:56:45 +0300

    to do list

---
+++ cyptomachine.py
@@ -0,0 +1,8 @@
+print("Project - crypto")
+
+# create keys string
+# autogenerate the values string by offsetting original string
+# create two dictionaries
+# user input "the message" and mode 
+# return result
+# clean and beautify the code 
