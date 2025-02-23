2f965eeb7402798be50f96f77a4f964d5863c988
kiganyamburu <mburukiganya@gmail.com>
2025-02-23 19:57:43 +0300

    enigma

--- cyptomachine.py
+++ cyptomachine.py
@@ -1,6 +1,7 @@
 print("Project - crypto")
-
+def enigma_light():
 # create keys string
+
 # autogenerate the values string by offsetting original string
 # create two dictionaries
 # user input "the message" and mode 
