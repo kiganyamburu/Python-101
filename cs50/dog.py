60de46265af62141592fe78d40c3f24dc295f179
kiganyamburu <mburukiganya@gmail.com>
2025-11-03 20:09:58 +0300

    feat(cs50): new classes

--- cs50/dog.py
+++ cs50/dog.py
@@ -3,6 +3,7 @@
 # 1. Start with a bus dictionary holding current passengers.
 
 
+
 #    - Each seat number (1, 2, 3, ...) is a key
 #    - Each value is another dictionary with each pet's:
 #        • name
